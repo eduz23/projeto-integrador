8ada37d6012907acf01ae607076f763eeae0e66e
Luiz Eduardo <10edu2008@gmail.com>
2025-02-04 21:34:11 -0300

    Add files via upload

---
+++ src/professor.ts
@@ -0,0 +1,26 @@
+export class Professor{
+    private id_professor: number
+    private nome_professor:string
+    private disciplina_professor:string
+    private telefone_professor:string
+
+    exibirAluno(){
+        
+    }
+
+    inserirAluno(){
+
+    }
+
+    encontrarAluno(){
+
+    }
+
+    deletarAluno(){
+
+    }
+
+    editarNota(){
+
+    }
+}
