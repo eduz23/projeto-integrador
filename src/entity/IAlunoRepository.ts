5430b2ab5ccdba6cbb54a0cb3e2d8ea7b0513e13
Luiz Eduardo <10edu2008@gmail.com>
2025-03-21 20:49:23 -0300

    Delete src/entity/IAlunoRepository.ts

--- src/entity/IAlunoRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Aluno } from "../entity/alunos";
-
-export interface IAlunosRepository{
-    listarAlunos(): Promise<Aluno[]>
-    buscarPorId(id: string): Promise<Aluno[]>
-    criarAluno(id: string, nome_aluno:string, idade_aluno:number, turma_aluno:string)
-    deletarAluno(id:string)
-    atualizarAluno(id:string, coluna:string, registro:string):Promise<void>
-}
