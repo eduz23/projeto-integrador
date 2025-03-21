e4bf1e7602ca6ebf3e2b6a948f9db84c76c3136e
Luiz Eduardo <10edu2008@gmail.com>
2025-03-21 20:47:46 -0300

    Add files via upload

--- src/entity/alunos.ts
+++ src/entity/alunos.ts
@@ -1,6 +1,9 @@
 const { Pool } = require('pg');
 
 export class Aluno{
+    id(id: any) {
+        throw new Error("Method not implemented.");
+    }
     private id_aluno: string
     private nome_aluno:string
     private idade_aluno:number
@@ -11,6 +14,10 @@ export class Aluno{
         this.nome_aluno = nome_aluno
         this.idade_aluno = idade_aluno
         this.turma_aluno = turma_aluno
+    }
+
+    public getId(){
+        return this.id_aluno
     }
 
 }
